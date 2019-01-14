647f8c7f0d45c1ba5fa94727c021d7bc242e0da0
rabindranathfv <rabindranathucv@gmail.com>
2019-01-14 11:35:53 -0300

    using dataGrid with http client and using filter and sort in the table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  title = 'Ag-Grid-Table';
+
+  columnDefs = [
+    {headerName: 'Make', field: 'make', sortable: true, filter: true },
+    {headerName: 'Model', field: 'model', sortable: true, filter: true },
+    {headerName: 'Price', field: 'price', sortable: true, filter: true }
+  ];
+
+  rowData: any;
+
+  constructor( private http: HttpClient ) {
+
+  }
+
+  ngOnInit() {
+        this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// libraries
+import { AgGridModule } from 'ag-grid-angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
