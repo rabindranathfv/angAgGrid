253978495ee21a5bd42a20fdd2faca3044f2e44a
rabindranathfv <rabindranathucv@gmail.com>
2019-01-15 18:03:41 -0300

    install, setup amCharts with angular v6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone, AfterViewInit, OnDestroy } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { AgGridNg2 } from 'ag-grid-angular';
+
+// amCharts import Libreary
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+am4core.useTheme(am4themes_animated);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  private chart: am4charts.XYChart;
   @ViewChild('agGrid') agGrid: AgGridNg2;
   private girdApi;
   private gridColumnApi;
@@ -81,13 +89,58 @@ export class AppComponent implements OnInit {
 
   rowData: any;
 
-  constructor( private http: HttpClient ) {
+  constructor( private http: HttpClient, private zone: NgZone ) {
 
   }
 
   ngOnInit() {
         // this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
         // this.rowData = this.http.get('https://api.myjson.com/bins/ly7d1');
+  }
+
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+      let chart = am4core.create('chartdiv', am4charts.XYChart);
+
+      chart.paddingRight = 20;
+
+      let data = [];
+      let visits = 10;
+      for (let i = 1; i < 366; i++) {
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+        data.push({ date: new Date(2018, 0, i), name: 'name' + i, value: visits });
+      }
+
+      chart.data = data;
+
+      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = chart.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = 'date';
+      series.dataFields.valueY = 'value';
+
+      series.tooltipText = '{valueY.value}';
+      chart.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      chart.scrollbarX = scrollbarX;
+
+      this.chart = chart;
+    });
+  }
+
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
+    });
   }
 
   getSelectedRows() {
